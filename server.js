9ba85a4d40d754e4eac71bbe109991cd5be0f9e8
Grace Ho <grace.ho@mcroiltools.com>
2021-04-14 10:45:17 -0500

    making sure I understand __dirname

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const path = require('path')
 //dependency from NPM 
 const express = require('express');
 
+
 //dependency from self created stuff
 
 
 
 const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
 
@@ -16,5 +20,10 @@ const PORT = process.env.PORT || 3001;
 app.get('/', (req, res) => {
     return res.sendFile(path.join(__dirname, 'hello.html'));
   });
+
+  app.post('/foo', (req, res) => {
+    const {Number} = req.body;  
+    res.json(Number);
+  })
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
