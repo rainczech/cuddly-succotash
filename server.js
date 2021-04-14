083b7978122e50a96367381d7749d7aadae3f54a
Grace Ho <grace.ho@mcroiltools.com>
2021-04-14 09:13:24 -0500

    added hello.html

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+//dependency from NODE standard library
+const path = require('path')
+
+//dependency from NPM 
 const express = require('express');
+
+//dependency from self created stuff
+
+
 
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+
+
 app.get('/', (req, res) => {
-    res.send('Welcome To Heroku!');
+    return res.sendFile(path.join(__dirname, 'hello.html'));
   });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
